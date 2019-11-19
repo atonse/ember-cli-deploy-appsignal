60b95c7768543345467649a1108d4ecf00010d9c
Ashish Tonse <ashish.tonse@gmail.com>
2019-11-19 13:52:25 -0500

    cleanup

--- index.js
+++ index.js
@@ -35,12 +35,8 @@ module.exports = {
         const requests = files.map(throat(1, file => {
           const cdnURL = `${assetMap.prepend}${assetMap.assets[file]}`;
           const mapFile = `${context.config.build.outputPath}/${assetMap.assets[file.replace(".js", ".map")]}`;
-          console.log(file, mapFile);
 
-          if (assetMap.assets[file.replace(".js", ".map")] === undefined) {
-            return;
-          }
-
+          if (assetMap.assets[file.replace(".js", ".map")] === undefined) { return; }
 
           const formData = {
             'name[]': cdnURL,
@@ -54,13 +50,6 @@ module.exports = {
         return Promise.all(requests)
                 .then(values => this.log('finished requests', values))
                 .catch((err) => this.log(err));
-
-      //   curl -k -X POST -H 'Content-Type: multipart/form-data' \
-      // -F 'name[]=https://d26keeai591nv2.cloudfront.net/assets/vendor-7958d9dcb7e60fe198d757abc7c2de5c.js' \
-      // -F 'revision=76013f44' \
-      // -F 'file=@tmp/deploy-dist/assets/vendor-c0be5e7e19658f8efd10dbfde3b4fc53.map' \
-      // 'https://appsignal.com/api/sourcemaps?push_api_key=${appsignalPushApiKey}&app_name=AccioAlert&environment=demo'
-
       }
     });
 
